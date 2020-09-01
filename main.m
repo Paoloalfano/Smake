34294c65fce112d13bd8afc889bf1d81c93ad7e3
Paoloalfano <62349821+Paoloalfano@users.noreply.github.com>
2020-09-01 22:29:08 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,4 +1,8 @@
 close all;clear; clc
+%<<<<<<<<<<<< output parameters >>>>>>>>>>>>>>>>>>>>>>>>
+dt=0.0025;      % dt of the synthetic accelerogram
+damping=0.05;      % damping to create the spectra to be compared
+nSequence=2;    % number of accelerograms to be created
+%<<<<<<<<<<< synthetiser parameters>>>>>>>>>>>>>>>>>>>>>
+dtSpectra=0.02; % dt for the spectra to be compared
 
-dt=0.0025;      %dt of the synthetic accelerogram
-Damp=[0.05];
